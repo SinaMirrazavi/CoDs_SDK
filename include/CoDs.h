3fadfe4c28f534f94808e84ad525297d6b604696
lasapc3 <sina.mirrazavi@epfl.ch>
2017-07-23 18:10:55 +0200

    The first eigen value is added but not tested!

--- include/CoDs.h
+++ include/CoDs.h
@@ -29,33 +29,64 @@
 using namespace std;
 using namespace Eigen;
 
+const double epsilon=10e-20;
+
 
 class CoDs
 {
 public:
-	void initialize(int Dimen_state,double delta_dx,double F_d,bool defined_surface);
+	void initialize(int Dimen_state,double delta_dx,double F_d,double Gammma_free_motion,bool defined_surface);
 	void Set_Gamma(double Gamma,VectorXd Normal,VectorXd q2,VectorXd q3);
-	void Set_State(VectorXd State,VectorXd Original_Dynamic);
+	void Set_State(VectorXd State,VectorXd DState,VectorXd Original_Dynamic);
+	void Set_Mass_Matrix(MatrixXd M);
+
+
+	MatrixXd Calculate_Modulation();
 
 
 private:
 
 	void Error();
+	inline	bool everythingisreceived();
+	inline	void everyfalse();
 
 	bool Surface_;
+	bool State_of_system_is_set_;
+	bool State_of_surface_is_set_;
 
 
 	int Dimen_state_;
 
 
 	double Gamma_Value_;
+	double Gammma_Threshold_;
+
+	double delta_dx_;
+	double F_d_;
+
+	double NF_;
+	double F_dNMN_;
 
 
 	VectorXd N_;
 	VectorXd q2_;
 	VectorXd q3_;
+
 	VectorXd X_;
+	VectorXd DX_;
 	VectorXd F_;
+
+	MatrixXd Q_;
+	MatrixXd Q_inv_;
+	MatrixXd M_;
+
+
+	MatrixXd InvMass_;
+
+
+	VectorXd Normal_velocity_robot_;
+
+
 
 
 };
--- src/CoDs.cpp
+++ src/CoDs.cpp
@@ -1,4 +1,3 @@
-
 /**
  *  @file    CoDs.cpp
  *  @author  Sina Mirrazavi (sina.mirrazavi@epfl.ch)
@@ -26,17 +25,61 @@
 /**
  *   @brief  Initializing the CoDs package.
  *
- *   @param  Dimen_state is the dimension of the state.
+ *   @param  Dimen_state is the dimension of the state. It should be 3!
  *   @param  delta_dx is the desired velocity of the robot at the impact point.
  *   @param  F_d is the desired force of the robot at the contact phase.
+ *   @param  Gammma_free_motion is the threshold which defines boundary between the free motion and the transition phases.
  *   @param  defined_surface is a boolean. If it is true, the package uses a planner surface defined in  the Gamma class. Otherwise it uses the user defined Gamma function.
  *   @return void
  */
-void CoDs::initialize(int Dimen_state,double delta_dx,double F_d, bool defined_surface)
+void CoDs::initialize(int Dimen_state,double delta_dx,double F_d,double Gammma_free_motion, bool defined_surface)
 {
 
+	if (Dimen_state==3)
+	{
+
+	}
+	else
+	{
+		cout<<"The dimension of the system should be 3; i.e. x y z!"<<endl;
+		cout<<"Dimen_state "<<Dimen_state<<endl;
+		Error();
+	}
+	if (0<=delta_dx)
+	{
+		cout<<"delta_dx should be negative! "<<delta_dx<<endl;
+		Error();
+	}
+	if (F_d<0)
+	{
+		cout<<"F_d should be positive! "<<F_d<<endl;
+		Error();
+	}
 	Dimen_state_=Dimen_state;
 	Surface_=defined_surface;
+	Gammma_Threshold_=Gammma_free_motion;
+
+	delta_dx_=delta_dx;
+	F_d_=F_d;
+
+
+
+	N_.resize(Dimen_state_);	q2_.resize(Dimen_state_);	q3_.resize(Dimen_state_);
+	X_.resize(Dimen_state_);	DX_.resize(Dimen_state_);	F_.resize(Dimen_state_);
+
+
+
+	Q_.resize(Dimen_state_,Dimen_state_); 			Q_.setZero();
+	Q_inv_.resize(Dimen_state_,Dimen_state_); 		Q_inv_.setZero();
+	M_.resize(Dimen_state_,Dimen_state_); 			M_.setZero();
+	InvMass_.resize(Dimen_state_,Dimen_state_); 	InvMass_.setIdentity();
+
+
+	Normal_velocity_robot_.resize(Dimen_state_); Normal_velocity_robot_.setZero();
+
+
+	everyfalse();
+
 }
 
 /**
@@ -48,11 +91,10 @@ void CoDs::initialize(int Dimen_state,double delta_dx,double F_d, bool defined_s
  *   @param  q3 is the tangential vector to N and q3;
  *   @return void
  */
-
 void CoDs::Set_Gamma(double Gamma,VectorXd Normal,VectorXd q2,VectorXd q3)
 {
 
-	if ((Normal.cols()==Dimen_state_)&&(q2.cols()==Dimen_state_)&&(q3.cols()==Dimen_state_)&&((N_.transpose()*q2_+N_.transpose()*q3_+q2.transpose()*q3_)(1,1)<0.0001))
+	if ((Normal.cols()==Dimen_state_)&&(q2.cols()==Dimen_state_)&&(q3.cols()==Dimen_state_)&&((N_.transpose()*q2_+N_.transpose()*q3_+q2.transpose()*q3_)(0,0)<0.0001))
 	{
 	}
 	else
@@ -71,37 +113,105 @@ void CoDs::Set_Gamma(double Gamma,VectorXd Normal,VectorXd q2,VectorXd q3)
 	N_=Normal;
 	q2_=q2;
 	q3_=q3;
+
+	Q_.col(0)=N_;	Q_.col(1)=q2_;	Q_.col(2)=q3_;
+
+	Q_inv_=Q_.inverse();
+	State_of_surface_is_set_=true;
+	cout<<"Q_ "<<Q_<<endl;
+	cout<<"Q_inv_"<<Q_inv_<<endl;
 }
 
 /**
- *   @brief Setting the state of the system and the original dynamic
+ *   @brief Setting the mass matrix of the robot, it should be in the Cartesian space and it should be 3$\times$3
  *
- *   @param  State is the state of the system.
- *   @param  Original_Dynamic is the original dynamic.
+ *   @param  State is the states of the system.
  *   @return void
  */
-
-void CoDs::Set_State(VectorXd State,VectorXd Original_Dynamic)
+void CoDs::Set_Mass_Matrix(MatrixXd M)
 {
 
-	if ((State.cols()==Dimen_state_)&&(Original_Dynamic.cols()==Dimen_state_))
+	if ((M.cols()==Dimen_state_)&&(M.rows()==Dimen_state_))
 	{
 	}
 	else
 	{
-		cout<<"Something is wrong in Set_State"<<endl;
-		cout<<"Dimension of states is: "<<Dimen_state_<<endl;
-		cout<<"State "<<State<<endl;
-		cout<<"Original_Dynamic "<<Original_Dynamic<<endl;
+		cout<<"Something is wrong in Set_Mass_Matrix"<<endl;
+		cout<<"Mass matrix "<<M<<endl;
 		Error();
 	}
 
-	X_=State;
-	F_=Original_Dynamic;
+	InvMass_=M.inverse();
 }
 
 
-//Gamma,f_x,DX,X,X_C,Q,Qinv,Contact
+
+
+/**
+ *   @brief Calculate the modulation function
+ *
+ *   @return The modulation function
+ */
+MatrixXd CoDs::Calculate_Modulation()
+{
+
+	if (!everythingisreceived())
+	{
+		cout<<"Even though you forgot setting sth, you called Calculate_Modulation"<<endl;
+		cout<<"State_of_system_is_set_ "<<State_of_system_is_set_<<endl;
+		cout<<"State_of_surface_is_set_ "<<State_of_surface_is_set_<<endl;
+		Error();
+	}
+
+	M_(0,0)=1; 	M_(1,1)=1; 	M_(2,2)=1;
+
+	if (Gammma_Threshold_<=Gamma_Value_)
+	{
+		M_(0,0)=(1-exp(-(Gamma_Value_-Gammma_Threshold_)));
+		M_(1,1)=M_(0,0);
+		M_(2,2)=M_(0,0);
+	}
+	if((Gamma_Value_<Gammma_Threshold_)&&(0<Gamma_Value_))
+	{
+		Normal_velocity_robot_=Q_.transpose()*DX_;
+		NF_=(N_.transpose()*F_)(0,0);
+
+		if (Normal_velocity_robot_(0)<delta_dx_)
+		{
+			M_(0,0)=(delta_dx_-Normal_velocity_robot_(0)+exp(-Gamma_Value_/epsilon))/(Gamma_Value_*NF_);
+		}
+		else
+		{
+			F_dNMN_=F_d_*(N_.transpose()*InvMass_*N_)(0,0)/NF_;
+			if ((delta_dx_<Normal_velocity_robot_(0))&&(Normal_velocity_robot_(0)<0))
+			{
+				M_(0,0)=-F_dNMN_*exp(-Gamma_Value_/epsilon);
+			}
+			else if (0<=Normal_velocity_robot_(0))
+			{
+				M_(0,0)=-F_dNMN_*(Normal_velocity_robot_(0)+exp(-Gamma_Value_/epsilon));
+			}
+
+
+
+		}
+	}
+	else if  (Gamma_Value_<=0)
+	{
+		NF_=(N_.transpose()*F_)(0,0);
+		F_dNMN_=-F_d_*(N_.transpose()*InvMass_*N_)(0,0)/NF_;
+		M_(0,0)=-F_dNMN_;
+	}
+
+
+	everyfalse();
+
+
+	return M_;
+
+}
+
+
 
 
 
@@ -122,6 +232,33 @@ void CoDs::Error()
 
 }
 
+/**
+ *   @brief  To check if everything has been correctly initialized or set.
+ *
+ *   @return A flag, if it is true, life is shiny.
+ */
+inline bool CoDs::everythingisreceived()
+{
+/*	bool flag=true;
+
+	flag=State_of_system_is_set_;
+	flag=State_of_surface_is_set_;*/
+
+	return ((State_of_system_is_set_)&&(State_of_surface_is_set_));
+
+}
+
+/**
+ *   @brief  To check if everything has been correctly initialized or set.
+ *
+ *   @return A flag, if it is true, life is shiny.
+ */
+inline void CoDs::everyfalse()
+{
+	State_of_system_is_set_=false;
+	State_of_system_is_set_=false;
+
+}
 
 
 
